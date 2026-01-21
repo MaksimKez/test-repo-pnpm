08a09d34f48a92dcf905037019f90322d16d07c2
maksim-kez <maksim.kez@innowise.com>
2026-01-21 15:43:30 +0100

    only mock tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
 import {isNumberObject} from "util/types";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 test('passes1', () => {
     expect(true).toBe(true);
 });
